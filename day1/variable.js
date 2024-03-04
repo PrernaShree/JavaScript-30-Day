a361019ff7906b988e90f83a6db6b8bbce8c4d11
PRERNA SHREE <146473030+PrernaShree@users.noreply.github.com>
2024-03-04 19:34:55 +0530

    Add files via upload

---
+++ day1/variable.js
@@ -0,0 +1,5 @@
+let city = "Patna" //string data type variable
+let isCompleted = true // boolean data type variable
+let myname
+console.log(myname) // undefined data type variable 
+let value = null
